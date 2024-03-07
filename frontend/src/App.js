9e5668b91a5d1632cfbd7d5f114860987f40b1fc
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-07 00:19:23 -0300

    fix: trying to fix accelerometer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import api from './api/api'
 import './App.css';
 
 import BeginScan from './components/begin_scan';
+import Accelerometer from './position/accelerometer';
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Hound</h1>
-      <BeginScan/>
+      <Accelerometer/>
     </div>
   );
 }
