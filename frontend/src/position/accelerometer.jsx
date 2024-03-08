ce2435c018ff3cbed104fee10e2ecbd3be29a6e3
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-08 00:55:57 -0300

    test: checking the problem

--- frontend/src/position/accelerometer.jsx
+++ frontend/src/position/accelerometer.jsx
@@ -113,6 +113,7 @@ function Accelerometer() {
   //the function receives the acceleration at that point on x, y and z, provided by the accelerometer and the last calculated position both as an array
   // acc_sum: the sum of all the accelerations minus the last one
   function toPosition(current_acc, last_acc, acc_sum, last_pos, time_elapsed) {
+    console.log('current_acc', current_acc);
     const current_pos = sum3d(...mult(sum3d(...mult(current_acc, 1/2), ...mult(last_acc, 3/2), ...mult(acc_sum, 2)), (time_elapsed^2)/2), ...last_pos);
     return(current_pos);
   }
