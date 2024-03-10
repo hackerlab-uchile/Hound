cb8b4ac38b76d8c1a315f3dfc4c97c884ceb5781
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-10 14:26:02 -0300

    fix: root_path error

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ from sqlalchemy import select, desc
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI()
+app = FastAPI(root_path = '/api/')
 
 # origins = [
 #     "http://localhost",
