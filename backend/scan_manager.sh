0ceb6bd9584014f3d7cbfdecc6048c1304b63529
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-11 13:48:18 -0300

    fix: automatization

--- backend/scan_manager.sh
+++ backend/scan_manager.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo 'Iniciando escaneo de red'
-
+export DISPLAY=:0
+export XAUTHORITY=/home/kali/.Xauthority 
 #mkfifo signalpipe
 
 # 'a' to toggle the scanning instance on airodump where they are set on the following order: AP+STA; AP+STA+ACK; AP only; STA only
@@ -8,6 +9,8 @@ airodump-ng wlan1
 # 2>&1 > fifo1 
 
 sleep 5
+
+
 
 xdotool key --delay 500 'a'
 xdotool key --delay 500 'a'
