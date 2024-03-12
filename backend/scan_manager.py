8a326769cc61565139d1051bb9258625667f5f50
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-12 12:26:28 -0300

    fix: data manipulation

--- backend/scan_manager.py
+++ backend/scan_manager.py
@@ -4,55 +4,49 @@ fifo_pipe = 'signalpipe'
 script_path = './scan_manager.sh'
 count = 0
 file = open('monitor.txt', 'w')
-
+array_stations = []
 
 def run_script():
     subprocess.call(['sh', script_path])
 
 def get_scannings():
-    array_stations = []
     with open(fifo_pipe, 'r') as signals:
         bssid = ""
         station = ""
         pwr = ""
-        i = 0
         while True: 
             line = signals.readline()
             if not line:
                 break
-            while (i < len(str(line))) :
-                if (str(line)[i] != " "):
-                    print(str(line)[i])
-                if (str(line)[i] == "(" ):
-                    bssid = "(not associated)"
-                    i += 15
-                if (str(line)[i]== ":" and i!=0):
-                    if (bssid != ""):
-                        print(str(line[i-2: i+14]))
-                        station = str(line[i-2: i+14])
-                    else:
-                        bssid = str(line[i-2: i+14])
-                        i += 14
-                if (str(line[i])== "-"):
-                    if (station != "" and pwr == ""):
-                        pwr = str(line[i: i+2])
-                        break
-                if (station != "" and pwr != "" and bssid != ""):
-                    array_stations.append({'bssid': bssid, 'station': station, 'pwr':pwr})
-                    bssid = ""
-                    station = ""
-                    pwr = ""
-                    i = 0
-                    break
-                i+=1
-    file.close()
+            array_stations.append(str(line))
+    print (array_stations)
 
 def parse_scannings():
-    with open(fifo_pipe, 'r') as signals:
-        while True: 
-            line = signals.readline()
-            if not line:
+    for i in range (0,len(array_stations)):
+        line = array_stations[i]
+        for i in range (0, len(str(line))) :
+            if (str(line)[i] != " "):
+                print(str(line)[i])
+            if (str(line)[i] == "(" ):
+                bssid = "(not associated)"
+                i += 15
+            if (str(line)[i]== ":" and i!=0):
+                if (bssid != ""):
+                    print(str(line[i-2: i+14]))
+                    station = str(line[i-2: i+14])
+                else:
+                    bssid = str(line[i-2: i+14])
+                    i += 14
+            if (str(line[i])== "-"):
+                if (station != "" and pwr == ""):
+                    pwr = str(line[i: i+2])
+                    break
+            if (station != "" and pwr != "" and bssid != ""):
+                array_stations.append({'bssid': bssid, 'station': station, 'pwr':pwr})
+                bssid = ""
+                station = ""
+                pwr = ""
+                i = 0
                 break
-            if (line.strip()[10] == " " and line.strip()[11] == "("):
-                print(f"parsed data: {line.strip()}")
-                
+            i+=1
+                    
