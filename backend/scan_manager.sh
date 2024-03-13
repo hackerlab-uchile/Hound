0a78919a37e753021cc65508a38eb742c716c7e3
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-13 12:03:35 -0300

    fix: automatization working fine!

--- backend/scan_manager.sh
+++ backend/scan_manager.sh
@@ -7,9 +7,7 @@ mkfifo signalpipe
 ## calling the airodump instance and the function to get the data from the pipe
 uuid="(uuidgen)" && tmux new -d -s "$uuid"
 tmux splitw -h -t "${uuid}:0.0"
-tmux send-keys -t "${uuid}.0" "cd Hound/backend" ENTER
 tmux send-keys -t "${uuid}.0" "expect scan_manager.exp" ENTER
-tmux send-keys -t "${uuid}.1" "cd Hound/backend" ENTER
-tmux send-keys -t "${uuid}.1" "import scan_manager; scan_manager.get_scannings()" ENTER
+tmux send-keys -t "${uuid}.1" "python -c 'import scan_manager; scan_manager.get_scannings()'" ENTER
 tmux a -t "$uuid"
 
