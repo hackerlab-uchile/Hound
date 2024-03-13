089ff896398a051f35f20156719efe57ad2d8391
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-13 20:45:14 -0300

    fix: one tmux was not closing

--- backend/stop_scan.sh
+++ backend/stop_scan.sh
@@ -6,7 +6,7 @@ tmux send-keys -t "$(uuid).0" C-c
 sleep 1
 tmux send-keys -t "$(uuid).0" "exit" ENTER
 
-tmux send-keys -t "$(uuid).0" "exit" ENTER
+tmux send-keys -t "$(uuid).1" "exit" ENTER
 
 
 echo 'Scan stopped'
