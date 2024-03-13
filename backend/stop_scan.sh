7ca2b6238489682947ac6aac21bfb5653796d2aa
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-13 12:18:29 -0300

    fix: closing terminals

--- backend/stop_scan.sh
+++ backend/stop_scan.sh
@@ -2,6 +2,10 @@
 
 echo 'Stopping scan...'
 
+uuid="(uuidgen)"
 tmux send-keys -t "$(uuid).0" C-c
+tmux send-keys -t "$(uuid).0" "exit" ENTER
+tmux send-keys -t "$(uuid).1" "exit" ENTER
+fclose(signalpipe)
 
 echo 'Scan stopped'
