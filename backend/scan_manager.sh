72480eaead85acd160a5675d61d645e7b39a6688
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-13 10:13:29 -0300

    fix: automatization

--- backend/scan_manager.sh
+++ backend/scan_manager.sh
@@ -5,10 +5,15 @@ mkfifo signalpipe
 
 
 ## calling the airodump instance and the function to get the data from the pipe
-tmux new -d session
+tmux new -d 
+
+tmux send -Rt 0 cd SPACE Hound ENTER
+tmux send -Rt 0 cd SPACE backend ENTER
 
 tmux send -Rt 0 expect SPACE scan_manager.exp ENTER
  
+python -c 'import scan_manager; scan_manager.get_scanings()'
+
 
 # 
 
