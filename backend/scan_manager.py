3ce8f137a3949eb3e96c60998b6dd1fd23cc2fe2
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-13 18:40:52 -0300

    fix: requests

--- backend/scan_manager.py
+++ backend/scan_manager.py
@@ -79,7 +79,7 @@ def parse_scannings(line):
                 
         if (station != "" and pwr != "" and bssid != ""):
             request_data = { 'network_scan_id': nwid, 'station': station, 'pwr':pwr }
-            request = requests.post(urlsignal, request_data)
+            request = requests.post(urlsignal, json.dumps(request_data))
             print(request_data)
             parsed_stations.append({ 'network_id': nwid, 'station': station, 'pwr':pwr })
             bssid = ""
