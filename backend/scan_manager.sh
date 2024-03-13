0da4937d49e5e44d670e25ca5960de916bbb964a
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-12 23:22:26 -0300

    fix: multiprocessing

--- backend/scan_manager.sh
+++ backend/scan_manager.sh
@@ -6,8 +6,8 @@ mkfifo signalpipe
 
 ## calling the airodump instance and the function to get the data from the pipe
 
-expect scan_manager.exp&
-python -c 'import scan_manager; scan_manager.get_scannings()'&
+nohup expect scan_manager.exp &
+nohup python -c 'import scan_manager; scan_manager.get_scannings()' &
 # 
 
 
