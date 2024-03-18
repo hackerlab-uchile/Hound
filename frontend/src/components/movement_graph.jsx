8628e479e05489e2276d41da7f06a58b199afa65
Natalia Quinteros <natalia.quinteros@ug.uchile.cl>
2024-03-18 12:18:34 -0300

    choreL adapting graph to show data

--- frontend/src/components/movement_graph.jsx
+++ frontend/src/components/movement_graph.jsx
@@ -3,9 +3,10 @@ import Plot from 'react-plotly.js';
 
 function MovementPlot() {
     const [data, setData] = useState([]);
-    var x = [1, 2, 3, 4, 5];
-    var y = [2, 3, 4, 5, 6];
-    var intensity = [10, 20, 80, 40, 50];
+    // var x = [1, 2, 3, 4, 5];
+    // var y = [2, 3, 4, 5, 6];
+    // var intensity = [10, 20, 80, 40, 50];
+
 
 
     useEffect(() => {
@@ -15,11 +16,20 @@ function MovementPlot() {
         .catch((error) => console.error('Error fetching data:', error));
     }, []);
 
+    const xValues = data.map((element)=> element.x)
+    const yValues = data.map((element)=> element.y)
+
+    var intensity = [];
+    for (var i = 0; i < xValues.length; i++) {
+        var randomColor = (Math.random(1, 100))
+        intensity.push(randomColor);
+    }
+
     const plotData = [{
-        // x: data.map((element)=> element.x),
-        // y: data.map((element)=> element.y),
-        x: x,
-        y: y,
+        x: xValues,
+        y: yValues,
+        // x: x,
+        // y: y,
         type: 'scattergl',
         mode: 'markers',
         mode: 'markers',
@@ -28,7 +38,7 @@ function MovementPlot() {
           colorscale: 'Viridis', // Set the colorscale ('Viridis' is just an example, you can use other built-in colorscales or define your own)
           cmin: 0, // Set the minimum value for the colorscale
           colorbar: {
-            title: 'Intensity' // Add a colorbar title
+            title: 'Signal Intensity' // Add a colorbar title
           }
         },
         color: intensity, 
